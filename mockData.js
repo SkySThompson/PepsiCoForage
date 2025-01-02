f2ea6f306c9d3287d7554815d5400713d732be44
Sky <skysabrinathompson@gmail.com>
2025-01-02 16:12:08 -0500

    Create mockData.js

---
+++ mockData.js
@@ -0,0 +1,18 @@
+export const distributors = [
+  {
+    name: "Distributor 1",
+    lastMonthShipment: 1200,
+    forecastedShipment: 1300,
+    yearToDateAverage: 1150,
+    shipmentTrend: [1100, 1200, 1300], // Last 3 months
+  },
+  {
+    name: "Distributor 2",
+    lastMonthShipment: 900,
+    forecastedShipment: 950,
+    yearToDateAverage: 870,
+    shipmentTrend: [900, 850, 950],
+  },
+  // Add more distributors as needed
+];
+
