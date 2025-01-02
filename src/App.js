7019a68c7fa8c5236d76e914a6c48b4516c746c9
Sky <skysabrinathompson@gmail.com>
2025-01-02 16:12:32 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Augur Dashboard</h1>
+      </header>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
